ae8d012fceaed8ed7959daf33b6e3d6ef5cae2cd
Idan Liberman <idanlib@gmail.com>
2023-01-05 14:41:35 +0200

    source audio modifications

--- src/sounds.js
+++ src/sounds.js
@@ -2,21 +2,21 @@ const smoothSounds = [
     {
         keyCode: 81,
         label: "Chord_1",
-        src: "../Samples/Chord_1.mp3",
+        src: "Samples/Chord_1.mp3",
         key: "Q",
         midiNote: 36
     },
     {
         keyCode: 87,
         label: "Chord_2",
-        src: "../Samples/Chord_2.mp3",
+        src: "Samples/Chord_2.mp3",
         key: "W",
         midiNote: 37
     },
     {
         keyCode: 69,
         label: "Chord_3",
-        src: "../Samples/Chord_3.mp3",
+        src: "Samples/Chord_3.mp3",
         key: "E",
         midiNote: 38
 
@@ -24,42 +24,42 @@ const smoothSounds = [
     {
         keyCode: 65,
         label: "Shaker",
-        src: "../Samples/Give_us_a_light.mp3",
+        src: "Samples/Give_us_a_light.mp3",
         key: "A",
         midiNote: 39
     },
     {
         keyCode: 83,
         label: "Open_HH",
-        src: "../Samples/Dry_Ohh.mp3",
+        src: "Samples/Dry_Ohh.mp3",
         key: "S",
         midiNote: 40
     },
     {
         keyCode: 68,
         label: "Closed_HH",
-        src: "../Samples/Bld_H1.mp3",
+        src: "Samples/Bld_H1.mp3",
         key: "D",
         midiNote: 41
     },
     {
         keyCode: 90,
         label: "Punchy_Kick",
-        src: "../Samples/punchy_kick_1.mp3",
+        src: "Samples/punchy_kick_1.mp3",
         key: "Z",
         midiNote: 42
     },
     {
         keyCode: 88,
         label: "Side_Stick",
-        src: "../Samples/side_stick_1.mp3",
+        src: "Samples/side_stick_1.mp3",
         key: "X",
         midiNote: 43
     },
     {
         keyCode: 67,
         label: "Snare",
-        src: "../Samples/Brk_Snr.mp3",
+        src: "Samples/Brk_Snr.mp3",
         key: "C",
         midiNote: 44
     }
